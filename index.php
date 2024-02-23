b4af0b3f290ef00fae037c0f751a765a56b4f3a9
Naveen Kumar3/IT/GGN/SPECTRA <naveen.kumar3@in.spectranet.com>
2024-02-23 15:53:16 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naveen's Portfolio </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+</head>
+
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="index.html">Portfo<span>lio.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Naveen Kumar</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="https://linkedin.com/in/naveenkr153" target="blank">Hire me</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/V1.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Naveen and I'm a <span class="typing-2"></span></div>
+                    <p>I'm a passionate web developer dedicated to crafting engaging online experiences. With a keen eye for design and a knack for coding, I specialize in creating dynamic and user-friendly websites that resonate with audiences. Proficient in HTML, CSS, JavaScript, and various frameworks, I thrive on turning ideas into reality, whether it's building responsive layouts or optimizing performance. Continuously learning and adapting to emerging technologies, I strive to stay ahead in the ever-evolving digital landscape. Committed to delivering high-quality solutions, I collaborate effectively with teams and clients to bring projects to fruition and exceed expectations.</p>
+                    <a href="https://drive.google.com/file/d/1zJUcj0oTpOcWBjsZHmpMBMTxC1ztmReJ/view?usp=sharing" target="blank">Download Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>Elevate your online presence with custom web design, responsive layouts, user-friendly experiences, e-commerce solutions, and ongoing maintenance and support.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>
+                            Boost your brand with tailored advertising solutions, captivating visuals, targeted campaigns, analytics-driven strategies, and continuous optimization for maximum impact.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>Enhance user engagement with intuitive app design, sleek interfaces, seamless navigation, performance optimization, and ongoing support for a standout digital experience.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>
+                        My creative journey spans diverse projects, showcasing adept problem-solving, innovative design solutions, and collaborative expertise. From crafting visually captivating graphics to developing user-friendly interfaces, my skills encompass a wide spectrum of creative endeavors. With a keen eye for detail and a passion for pushing boundaries, I thrive in challenging environments, delivering impactful results that exceed expectations. Through continuous learning and adaptability, I remain committed to honing my craft and bringing fresh perspectives to every project I undertake.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/AgraYamuna.jpg" alt="">
+                        <div class="text">Naveen</div>
+                        <p>Creative problem solver with innovative design skills and collaborative expertise.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/V1.jpg" alt="">
+                        <div class="text">Naveen</div>
+                        <p>Creative problem solver with innovative design skills and collaborative expertise.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/Munnu.JPG" alt="">
+                        <div class="text">Praveen</div>
+                        <p>Creative problem solver with innovative design skills and collaborative expertise.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-4.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Creative problem solver with innovative design skills and collaborative expertise.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-5.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Creative problem solver with innovative design skills and collaborative expertise.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Let's embark on a journey of learning together. Share your knowledge, explore new horizons, and grow together. Join me in fostering a community of continuous learning and collaboration. Together, we can achieve greatness!</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Naveen Kumar</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Saket, New Delhi</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">naveenkr153@yahoo.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="send_mail.php" method="post">
+                        <div class="fields">
+                            <div class="field name">
+                                <input name="name" type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input name="email" type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input name="subject" type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea name="text_area" cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit" style = "cursor: not-allowed">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="linkedin.com/in/naveenkr153">Naveen Kumar</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js">
+function sendMail(){
+    alert("werty");
+}
+
+    </script>
+</body>
+</html>
