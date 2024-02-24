4b3045849a60b011428a025af2e9d9643fe07175
Naveen Kumar <79682298+naveenkr153@users.noreply.github.com>
2024-02-24 20:47:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Editer", "Developer", "Designer", "Freelancer", "Blogger"],
+        strings: ["Editor", "Developer", "Designer", "Freelancer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Editer", "Developer", "Designer", "Freelancer", "Blogger"],
+        strings: ["Editor", "Developer", "Designer", "Freelancer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
